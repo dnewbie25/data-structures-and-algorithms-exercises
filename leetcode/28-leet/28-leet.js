5c8ca57cc3781f8cdf1cf8ca6565254e40e92e69
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2024-10-04 11:47:25 -0500

    Create 28-leet.js

---
+++ leetcode/28-leet/28-leet.js
@@ -0,0 +1,13 @@
+var once = function(fn) {
+    let functionCalled = false
+    let result
+    return function(...args){
+       if(!functionCalled){
+        result = fn(...args)
+        functionCalled = true
+        return result
+       }else{
+        return undefined
+       }
+    }
+};
